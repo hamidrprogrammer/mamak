d65191b88b230940c4f22e0ce5923c6210f92917
hamidrprogrammer <hamidrprogrammer@gmail.com>
2024-11-07 06:33:03 +0000

    Update BaseUrls.dart

--- lib/config/apiRoute/BaseUrls.dart
+++ lib/config/apiRoute/BaseUrls.dart
@@ -1,5 +1,5 @@
 class BaseUrls {
-  static const baseUrl = "landing.mamakschool.ir";
+  static const baseUrl = "back.mamakschool.ir";
   // static const baseUrl = "62.106.95.127:7000";
   static const storagePath = "https://static.mamakschool.ir/MamakStaticFiles";
   static const basePath = "MamakTestBackend/api";
